3a99da8224cc6d023fbf7bac546a5746a87ce3af
Joseph-Caballero <joseph.w.caballero@gmail.com>
2023-09-07 11:00:52 -0400

    added NewSurvey struct

--- programs/survey/src/lib.rs
+++ programs/survey/src/lib.rs
@@ -1,4 +1,5 @@
 use anchor_lang::prelude::*;
+use anchor_spl::token_interface::TokenInterface;
 
 declare_id!("sMafYZrzDNACfmwEDyzgCv7Bq1NKWy8CkkgNjKXRD3f");
 
@@ -11,5 +12,22 @@ pub mod survey {
     }
 }
 
+// TODO: For text fields of survey we can use noop.
+// https://github.com/solana-labs/solana-program-library/tree/master/account-compression/programs/noop
+
 #[derive(Accounts)]
 pub struct Initialize {}
+
+#[derive(Accounts)]
+pub struct NewSurvey<'info> {
+    #[account(mut)]
+    pub user: Signer<'info>,
+    //user ata
+    pub vault: InterfaceAccount<'info, TokenAccount>,
+
+    pub token_program: Interface<'info, TokenInterface>,
+}
+
+//when a user fills out a survey
+//encrypt to survey creators key
+//store on noop. that way survey data is still private for each user
