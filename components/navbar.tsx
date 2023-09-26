2db8adaa232cd32b2cbe6c21bf3848aef47fda83
Joseph-Caballero <joseph.w.caballero@gmail.com>
2023-09-26 14:39:12 -0400

    updated styling on logo & button

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 const Navbar = () => {
   return (
     <div className="w-full h-28 flex justify-around items-center max-w-7xl">
-      <div className="">
+      <div className="ml-4">
         <Image src="/formigo_logo.png" alt="formigo logo" width={60} height={12}/>
       </div>
       <div className="flex justify-around items-center w-1/2 text-lg">
@@ -12,7 +12,7 @@ const Navbar = () => {
         <Link href="">Features</Link>
         <Link href="">FAQ</Link>
       </div>
-      <button className="border-2 bg-[#0B44ED] p-4 text-lg text-white rounded-xl">Login Account</button>
+      <button className="mr-4 bg-formigo-blue p-3 text-lg text-white rounded-xl">Login Account</button>
     </div>)
 }
 
