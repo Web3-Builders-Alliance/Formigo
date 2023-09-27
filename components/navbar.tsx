f23e782145b337b20eee73b934593ae65bfda5d5
Joseph-Caballero <joseph.w.caballero@gmail.com>
2023-09-27 17:02:56 -0400

    updated navbar styling

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -3,17 +3,29 @@ import Link from "next/link";
 
 const Navbar = () => {
   return (
-    <div className="w-full h-28 flex justify-around items-center max-w-7xl">
-      <div className="ml-4">
-        <Image src="/formigo_logo.png" alt="formigo logo" width={60} height={12}/>
+    <div className="sm:h-36 flex w-5/6 items-center justify-between xs:h-24">
+      <div className="flex w-[14%] min-w-[10%] max-w-[100px] justify-start">
+        <Image
+          src="/formigo_logo.png"
+          alt="formigo logo"
+          width={65}
+          height={12}
+        />
       </div>
-      <div className="flex justify-around items-center w-1/2 text-lg">
-        <Link href="">About</Link>
-        <Link href="">Features</Link>
-        <Link href="">FAQ</Link>
+      <div className="sm:text-lg flex w-1/2 max-w-md items-center justify-around font-semibold xs:text-sm">
+        <Link href="" className="rounded-xl p-1 hover:bg-formigo-grey">
+          About
+        </Link>
+        <Link href="" className="rounded-xl p-1 hover:bg-formigo-grey">Features</Link>
+        <Link href="" className="rounded-xl p-1 hover:bg-formigo-grey">FAQ</Link>
       </div>
-      <button className="mr-4 bg-formigo-blue p-3 text-lg text-white rounded-xl">Login Account</button>
-    </div>)
-}
+      <div className="flex w-2/12 justify-end">
+        <button className="sm:w-1/2 sm:text-lg min-w-fit rounded-xl bg-formigo-blue p-3 text-white hover:brightness-110 xs:w-fit xs:text-xs">
+          Login
+        </button>
+      </div>
+    </div>
+  );
+};
 
 export default Navbar;
