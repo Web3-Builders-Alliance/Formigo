60358a6859b653e99c3dbbe1688b7bb151e5d23d
Joseph-Caballero <joseph.w.caballero@gmail.com>
2023-09-28 15:05:22 -0400

    updated landing style

--- components/Landing.tsx
+++ components/Landing.tsx
@@ -1,6 +1,6 @@
 function Landing() {
   return (
-    <div className="grid h-full grid-flow-col gap-8 pt-4 pb-4 w-full xs:grid-cols-1 xs:grid-rows-1 xs:pt-36 lg:w-5/6 lg:grid-cols-6 lg:grid-rows-6 lg:pt-8">
+    <div className="grid h-full grid-flow-col gap-8 pt-4 pb-4 xs:grid-cols-1 xs:grid-rows-1 xs:pt-36 lg:w-5/6 lg:grid-cols-6 lg:grid-rows-6 lg:pt-8">
       <div className="col-span-3 row-span-3 flex w-full flex-col justify-around xs:h-4/6  xs:items-center lg:h-full lg:items-start">
         <h3 className="xs:p-4 lg:pt-4 lg:pb-4 lg:pl-0 lg:pr-0 font-bold xs:text-center xs:text-5xl xs:max-w-md lg:max-w-3xl lg:text-left lg:text-6xl">
           Crafting Survey Forms With Flair
