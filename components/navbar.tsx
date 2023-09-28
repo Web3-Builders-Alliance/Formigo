4f82026df058abd2d1dcf38bc2fadf6ea3259468
Joseph-Caballero <joseph.w.caballero@gmail.com>
2023-09-28 15:05:59 -0400

    added link functionality

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,25 +1,31 @@
+"use client"
+
 import Image from "next/image";
 import Link from "next/link";
 
 const Navbar = () => {
   return (
     <div className="sm:h-36 flex w-5/6 items-center justify-between xs:h-24">
-      <div className="flex w-[14%] min-w-[10%] max-w-[100px] justify-start">
+      <Link href="/" className="flex w-[14%] min-w-[10%] justify-start">
         <Image
           src="/formigo_logo.png"
           alt="formigo logo"
           width={65}
           height={12}
         />
-      </div>
+      </Link>
       <div className="sm:text-lg flex w-1/2 max-w-md items-center justify-around font-semibold xs:text-sm">
-        <Link href="" className="rounded-xl p-1 hover:bg-formigo-grey">
+        <Link href="/about" className="rounded-xl p-1 hover:bg-formigo-grey">
           About
         </Link>
-        <Link href="" className="rounded-xl p-1 hover:bg-formigo-grey">Features</Link>
-        <Link href="" className="rounded-xl p-1 hover:bg-formigo-grey">FAQ</Link>
+        <Link href="/features" className="rounded-xl p-1 hover:bg-formigo-grey">
+          Features
+        </Link>
+        <Link href="/faq" className="rounded-xl p-1 hover:bg-formigo-grey">
+          FAQ
+        </Link>
       </div>
-      <div className="flex w-2/12 justify-end">
+      <div className="flex w-[14%] justify-end">
         <button className="sm:w-1/2 sm:text-lg min-w-fit rounded-xl bg-formigo-blue p-3 text-white hover:brightness-110 xs:w-fit xs:text-xs">
           Login
         </button>
