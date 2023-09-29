71f22a55e6b4c26b7ed5df5ce9b0703e6adfa083
Joseph-Caballero <joseph.w.caballero@gmail.com>
2023-09-29 16:16:08 -0400

    updated navbar spacing

--- components/HomeNavBar.tsx
+++ components/HomeNavBar.tsx
@@ -5,7 +5,7 @@ import Link from "next/link";
 
 const HomeNavbar = () => {
   return (
-    <div className="sm:h-36 flex w-5/6 items-center justify-between xs:h-24">
+    <div className="sm:h-36 mt-4 mb-4 flex w-5/6 items-center justify-between xs:h-24">
       <Link href="/" className="flex w-[14%] min-w-[10%] justify-start">
         <Image
           src="/formigo_logo.png"
@@ -14,11 +14,11 @@ const HomeNavbar = () => {
           height={12}
         />
       </Link>
-      <div className="sm:text-lg flex xs:w-1/2 sm:w-8/12 text-center xs:max-w-md sm:max-w-full items-center sm:justify-start xs:justify-around font-semibold xs:text-sm">
-        <Link href="/surveys" className="rounded-xl p-1 xs:w-fit sm:w-2/12 hover:bg-formigo-grey">
+      <div className="sm:text-2xl flex xs:w-1/2 sm:w-8/12 text-center xs:max-w-md sm:max-w-full items-center justify-start font-semibold xs:text-sm">
+        <Link href="/surveys" className="rounded-xl p-4 xs:w-fit sm:w-3/12 text-center hover:bg-formigo-grey">
           Surveys
         </Link>
-        <Link href="/responses" className="rounded-xl p-1 xs:w-fit sm:w-2/12 hover:bg-formigo-grey">
+        <Link href="/responses" className="rounded-xl p-4 xs:w-fit sm:w-3/12 text-center hover:bg-formigo-grey">
           Responses
         </Link>
       </div>
