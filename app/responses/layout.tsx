4125a966f255cd0e5c8c30a11512c242d6629e63
Joseph-Caballero <joseph.w.caballero@gmail.com>
2023-09-28 22:13:48 -0400

    init responses

---
+++ app/responses/layout.tsx
@@ -0,0 +1,17 @@
+import Footer from "@/components/Footer";
+import HomeNavbar from "@/components/HomeNavBar";
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className="flex h-screen w-screen flex-col border-2 justify-between items-center">
+        <HomeNavbar/>
+        {children}
+        <Footer/>
+      </body>
+    </html>
+  )};
---
+++ components/Responses.tsx
@@ -0,0 +1,5 @@
+const Responses = () => {
+  return (<div>Responses</div>)  
+}
+
+export default Responses;
