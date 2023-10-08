0e826bff348b4c2df9bfb3454078df4cd053278b
Raymart <exidz@protonmail.com>
2023-10-08 10:08:09 +0800

    feat:editor page

---
+++ frontend/app/editor/layout.tsx
@@ -0,0 +1,11 @@
+import NavEditor from "@/components/nav-editor";
+
+
+export default function Layout({ children }: { children: React.ReactNode }) {
+  return (
+    <div className="flex min-h-screen flex-col">
+      <NavEditor />
+      {children}
+    </div>
+  );
+}
