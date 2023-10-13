11e60f305b0f3e0c675bad85f56b10f2ca3ea9cd
Joseph-Caballero <joseph.w.caballero@gmail.com>
2023-10-12 20:38:53 -0400

    init phantom integration

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,32 +1,34 @@
-import DividerText from "@/components/divider-text";
-import MagicInput from "@/components/magic-input";
-import PhantomButton from "@/components/phantom-button";
-import Image from "next/image";
+'use client';
+
+import DividerText from '@/components/divider-text';
+import MagicInput from '@/components/magic-input';
+import PhantomButton from '@/components/phantom-button';
+import Image from 'next/image';
 
 export default function Home() {
   return (
-    <main className="flex h-screen flex-col items-center justify-center font-sans">
-      <div className="flex flex-col h-fit w-2/5 bg-card rounded-md px-[70px] py-[48px]">
-        <div className="flex">
+    <main className='flex h-screen flex-col items-center justify-center font-sans'>
+      <div className='flex h-fit w-2/5 flex-col rounded-md bg-card px-[70px] py-[48px]'>
+        <div className='flex'>
           <Image
-            src="/formigo_logo_white.png"
+            src='/formigo_logo_white.png'
             width={36}
             height={0}
-            className="mr-2"
-            alt="Formigo Logo"
+            className='mr-2'
+            alt='Formigo Logo'
           />
-          <h1 className="font-serif text-2xl font-semibold">Formigo</h1>
+          <h1 className='font-serif text-2xl font-semibold'>Formigo</h1>
         </div>
-        <p className="text-txt-secondary text-base font-medium mt-6 text-left">
+        <p className='mt-6 text-left text-base font-medium text-txt-secondary'>
           In order to use Formigo you are required to sign in by selecting from
           the available login methods below.
         </p>
-        <div className="mt-6 w-full">
+        <div className='mt-6 w-full'>
           <MagicInput />
         </div>
-        <DividerText label="or" />
-        <div className="w-full">
-          <PhantomButton className="w-full" />
+        <DividerText label='or' />
+        <div className='w-full flex justify-center'>
+          <PhantomButton/>
         </div>
       </div>
     </main>
--- frontend/components/phantom-button.tsx
+++ frontend/components/phantom-button.tsx
@@ -1,17 +1,54 @@
-import PhantomIcon from "./icons/phantom";
-import { Button } from "./ui/button";
+'use client';
 
-export default function PhantomButton({
-  className,
-  onClick,
-}: {
-  className?: string;
-  onClick?: () => void;
-}) {
-  return (
-    <Button variant="outline" className={className} onClick={onClick}>
-      <PhantomIcon className="w-5 h-4 mr-2" />
-      Phantom wallet
-    </Button>
+import React, { FC, useCallback, useMemo } from 'react';
+import {
+  ConnectionProvider,
+  WalletProvider,
+} from '@solana/wallet-adapter-react';
+import { Adapter, WalletAdapterNetwork } from '@solana/wallet-adapter-base';
+import { UnsafeBurnerWalletAdapter } from '@solana/wallet-adapter-wallets';
+import {
+  WalletModalProvider,
+  WalletDisconnectButton,
+  WalletMultiButton,
+  BaseWalletMultiButton,
+} from '@solana/wallet-adapter-react-ui';
+import { clusterApiUrl } from '@solana/web3.js';
+import { SolanaSignInInput } from '@solana/wallet-standard-features';
+
+// Default styles that can be overridden by your app
+require('@solana/wallet-adapter-react-ui/styles.css');
+
+const LABELS = {
+  'change-wallet': 'Change wallet',
+  connecting: 'Connecting ...',
+  'copy-address': 'Copy address',
+  copied: 'Copied',
+  disconnect: 'Disconnect',
+  'has-wallet': 'Connect',
+  'no-wallet': 'Select Wallet',
+} as const;
+
+export const PhantomButton: FC = () => {
+  const network = WalletAdapterNetwork.Devnet;
+
+  const endpoint = useMemo(() => clusterApiUrl(network), [network]);
+
+  const wallets = useMemo(
+    () => [],
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    [network]
   );
-}
+
+  return (
+    <ConnectionProvider endpoint={endpoint}>
+      <WalletProvider wallets={wallets} autoConnect>
+        <WalletModalProvider>
+            <WalletMultiButton />
+        </WalletModalProvider>
+      </WalletProvider>
+    </ConnectionProvider>
+  );
+};
+
+export default PhantomButton;
