b616c3545d979fed8d7850105d43fae5bbd5fc82
Raymart <exidz@protonmail.com>
2023-10-14 18:21:07 +0800

    add status

--- backend/src/models/form.model.ts
+++ backend/src/models/form.model.ts
@@ -5,13 +5,17 @@ export interface IForm extends Document {
   formId: string;
   totalChunk: number;
   views: number;
-  iv: string
+  name: string;
+  status: "active" | "draft" | "archive" | "disable";
+  iv: string;
 }
 
 const formSchema = new mongoose.Schema(
   {
     formId: { type: String, unique: true, required: true },
     creator: { type: String, required: true },
+    name: { type: String, required: true },
+    status: { type: String, default: "active" },
     iv: { type: String, required: true },
     totalChunk: { type: Number, required: true },
     views: { type: Number, default: 0 },
